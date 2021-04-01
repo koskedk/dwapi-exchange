e2f7eeda7d99d04a10fcc65b37b988aacfba3244
Koske Kimutai <koskedk@live.com>
2021-04-01 17:48:35 +0300

    updated

--- src/Dwapi.Exchange.SharedKernel/Custom/Utils.cs
+++ src/Dwapi.Exchange.SharedKernel/Custom/Utils.cs
@@ -1,5 +1,4 @@
 using System;
-using Dwapi.Exchange.SharedKernel.Model;
 
 namespace Dwapi.Exchange.SharedKernel.Custom
 {
@@ -18,22 +17,6 @@ namespace Dwapi.Exchange.SharedKernel.Custom
             }
 
             return 0;
-        }
-
-        public static ExtractBlock CreateBlock(long pageNumber, long pageSize)
-        {
-            pageNumber = pageNumber < 0 ? 1 : pageNumber;
-            pageSize = pageSize < 0 ? 1 : pageSize;
-
-            /*
-              Offset = (pageNumber - 1) * pageSize,
-                        PageSize = pageSize
-             */
-
-            var first = pageNumber == 1 ? pageNumber : (pageNumber - 1) * pageSize + 1;
-            var last = (first + pageSize) - 1;
-
-            return new ExtractBlock(first, last);
         }
     }
 }
